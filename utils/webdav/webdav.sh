06c91198a2b326472705034c6e225b4fc22fa101
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-02-23 00:11:08 +0800

    Update webdav.sh

--- utils/webdav/webdav.sh
+++ utils/webdav/webdav.sh
@@ -2,18 +2,18 @@
 set -e
 
 PUID=${PUID:-1000}
-PGID=${PGID:-1000}
+#PGID=${PGID:-1000}
 
 # add PUID:PGID, ignore error
-addgroup -g $PGID -S user-group 1>/dev/null || true
+#addgroup -g $PGID -S user-group 1>/dev/null || true
 adduser -u $PUID -S user 1>/dev/null || true
 
 chown $PUID:$PGID "/etc/webdav/"
 chown $PUID:$PGID "/data"
 
 # Set umask
-if [ "x$PUMASK" != "x" ]; then
-    umask $PUMASK
-fi
+#if [ "x$PUMASK" != "x" ]; then
+#    umask $PUMASK
+#fi
 
 exec webdav "$@"
