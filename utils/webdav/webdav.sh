07656b9abbd8d999730e18be940aa8191cf7a10b
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-02-22 23:25:07 +0800

    Update webdav.sh

--- utils/webdav/webdav.sh
+++ utils/webdav/webdav.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+
 # add PUID:PGID, ignore error
 addgroup -g $PGID -S user-group 1>/dev/null || true
 adduser -u $PUID -S user 1>/dev/null || true
