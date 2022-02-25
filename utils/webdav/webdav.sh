0aa136024f00d8ea10dbe51fe6cd5d5074b2abc8
pexcn <i@pexcn.me>
2022-02-25 15:59:07 +0800

    update env var name

--- utils/webdav/webdav.sh
+++ utils/webdav/webdav.sh
@@ -9,6 +9,6 @@ _is_exist_user() {
   id -u $1 &>/dev/null
 }
 
-_is_exist_group webdav || addgroup -S -g $GID webdav || true
-_is_exist_user webdav || adduser -S -G webdav -s /bin/ash -u $UID webdav || true
+_is_exist_group webdav || addgroup -S -g $PGID webdav || true
+_is_exist_user webdav || adduser -S -G webdav -s /bin/ash -u $PUID webdav || true
 exec su webdav -c webdav "$@"
