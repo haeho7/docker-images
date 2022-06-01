c4432eee485254af3970c9f8196a0b4b23078f4e
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-06-02 00:41:16 +0800

    update

---
+++ utils/nextcloud/nextcloud.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+# set env
+USER=www-data
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+UMASK=${UMASK:-000}
+
+# set user www-data to specified user id (non unique)
+usermod -o -u "${PUID}" -g users -G ${USER} -s /bin/ash ${USER}
+
+# set group users to specified group id (non unique)
+groupmod -o -g "${PGID}" users &>/dev/null
+
+#set folder's owne
+# chown $PUID:$PGID "/etc/webdav"
+# chown $PUID:$PGID "/data"
+
+# if UMASK is not null set umask
+if [ "x$UMASK" != "x" ]; then
+    umask $UMASK
+fi
+
+# exec su -s /bin/ash www-data -c webdav "$@"
+# exec su -s /bin/ash www-data -c umask $UMASK -c /entrypoint.sh "$@"
+
+gosu ${USER} -c umask $UMASK -c /entrypoint.sh "$@"
+
