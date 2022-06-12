7083b3626d5ef0da1255da22370f11bc8ec3ccfa
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-06-12 19:22:16 +0800

    build mariadb

--- ext/mariadb/mariadb.sh
+++ ext/mariadb/mariadb.sh
@@ -2,7 +2,7 @@
 set -e
 
 # set env
-USER=redis
+USER=mysql
 GROUP=users
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
@@ -13,9 +13,10 @@ usermod -o -u ${PUID} -g ${GROUP} -G ${USER} -s /bin/ash ${USER}
 # set group users to specified group id (non unique)
 groupmod -o -g ${PGID} ${GROUP} &>/dev/null
 
-# copy and chown redis.conf
-cp -a /tmp/redis.conf /data
-chown ${PUID}:${PGID} /data/redis.conf
+# copy and chown mariadb.cnf
+cp -a /tmp/mariadb.cnf /etc/mysql/conf.d
+chown ${PUID}:${PGID} /etc/mysql/conf.d/mariadb.cnf
+chmod 0644 /etc/mysql/conf.d/mariadb.cnf
 
-# call redis official startup script
+# call mariadb official startup script
 exec /usr/local/bin/docker-entrypoint.sh "$@"
