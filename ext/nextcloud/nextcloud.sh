d308fd78b4286376c4594c91d2a1842b406225cc
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-06-13 22:53:04 +0800

    Update nextcloud.sh

--- ext/nextcloud/nextcloud.sh
+++ ext/nextcloud/nextcloud.sh
@@ -4,12 +4,12 @@ set -e
 # set env
 USER=www-data
 GROUP=users
-GROUPS=www-data,tty
+GROUPS=www-data
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
 # set user to specified user id (non unique)
-usermod -o -u ${PUID} -g ${GROUP} -G ${GROUPS} -s /bin/bash ${USER} &>/dev/null
+usermod -o -u ${PUID} -g ${GROUP} -G ${GROUPS} -s /bin/ash ${USER} &>/dev/null
 
 # set group users to specified group id (non unique)
 groupmod -o -g ${PGID} ${GROUP} &>/dev/null
@@ -17,7 +17,7 @@ groupmod -o -g ${PGID} ${GROUP} &>/dev/null
 # chown nextcloud folder
 # See more: https://github.com/nextcloud/docker/blob/00d88733d0d93d0abc628c7b18bc589181c34cbb/24/fpm-alpine/Dockerfile#L100
 chown -R ${PUID}:${PGID} /var/www
-chown -R ${USER} /proc/self/fd/
  
 # call nextcloud official startup script
-exec gosu ${USER} /entrypoint.sh "$@"
+# exec gosu ${USER} /entrypoint.sh "$@"
+exec /entrypoint.sh "$@"
