a42c94d3bb7b4a544c2366929a9d38a8e2e7f291
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-06-13 16:22:18 +0800

    update

--- utils/webdav/webdav.sh
+++ utils/webdav/webdav.sh
@@ -10,14 +10,14 @@ PGID=${PGID:-1000}
 UMASK=${UMASK:-000}
 
 # set user to specified user id (non unique)
-usermod -o -u ${PUID} -g ${GROUP} -G ${GROUPS} -s /bin/bash ${USER} &>/dev/null
+usermod -o -u ${PUID} -g ${GROUP} -G ${GROUPS} -s /bin/ash ${USER} &>/dev/null
 
 # set group users to specified group id (non unique)
 groupmod -o -g ${PGID} ${GROUP} &>
 
 #set folder's owne
-chown $PUID:$PGID "/etc/webdav"
-chown $PUID:$PGID "/data"
+chown $PUID:$PGID /etc/webdav
+chown $PUID:$PGID /data
 
 # if UMASK is not null set umask
 if [ "x$UMASK" != "x" ]; then
