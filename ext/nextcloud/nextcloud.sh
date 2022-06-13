3d5c22042e35c4ddf9a4d398f786c9fb087abb5b
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-06-13 22:44:01 +0800

    Update nextcloud.sh

--- ext/nextcloud/nextcloud.sh
+++ ext/nextcloud/nextcloud.sh
@@ -4,7 +4,7 @@ set -e
 # set env
 USER=www-data
 GROUP=users
-GROUPS=www-data
+GROUPS=www-data,tty
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
@@ -17,6 +17,7 @@ groupmod -o -g ${PGID} ${GROUP} &>/dev/null
 # chown nextcloud folder
 # See more: https://github.com/nextcloud/docker/blob/00d88733d0d93d0abc628c7b18bc589181c34cbb/24/fpm-alpine/Dockerfile#L100
 chown -R ${PUID}:${PGID} /var/www
+chown -R ${USER} /proc/self/fd/
  
 # call nextcloud official startup script
 exec gosu ${USER} /entrypoint.sh "$@"
