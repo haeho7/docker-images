c02c51db092d14f15c0192ef36f20914fb3ddf42
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-06-14 18:57:52 +0800

    update

--- ext/nextcloud/nextcloud.sh
+++ ext/nextcloud/nextcloud.sh
@@ -13,6 +13,7 @@ usermod -o -u ${PUID} -g ${GROUP} -G ${GROUPS} -s /bin/ash ${USER} &>/dev/null
 
 # set group users to specified group id (non unique)
 groupmod -o -g ${PGID} ${GROUP} &>/dev/null
+groupmod -o -g ${PGID} ${GROUPS} &>/dev/null
 
 # chown nextcloud folder
 # See more: https://github.com/nextcloud/docker/blob/00d88733d0d93d0abc628c7b18bc589181c34cbb/24/fpm-alpine/Dockerfile#L100
