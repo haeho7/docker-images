df1e1d0a1be13d2ac8750743338ec5ca62623265
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-06-15 17:18:38 +0800

    update

--- ext/mariadb/mariadb.sh
+++ ext/mariadb/mariadb.sh
@@ -3,21 +3,21 @@ set -e
 
 # set env
 USER=mysql
-GROUP=users
-GROUPS=mysql
+GROUP=mysql
+GROUPS=mysql,users
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
 # set user to specified user id (non unique)
-usermod -o -u ${PUID} -g ${GROUP} -G ${GROUPS} -s /bin/bash ${USER} &>/dev/null
+usermod -o -u ${PUID} -g ${GROUP} -aG ${GROUPS} -s /bin/bash ${USER} &>/dev/null
 
 # set group users to specified group id (non unique)
 groupmod -o -g ${PGID} ${GROUP} &>/dev/null
 
 # copy and chown mariadb.cnf
-cp -a /tmp/mariadb.cnf /etc/mysql/conf.d/
-chmod 644 /etc/mysql/conf.d/mariadb.cnf
-chown ${PUID}:${PGID} /etc/mysql/conf.d/mariadb.cnf
+# cp -a /tmp/mariadb.cnf /etc/mysql/conf.d/
+chown -R ${PUID}:${PGID} /etc/mysql/conf.d/
+chmod -R 644 /etc/mysql/conf.d/
 
 # call mariadb official startup script
 exec /usr/local/bin/docker-entrypoint.sh "$@"
