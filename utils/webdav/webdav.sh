fe179e306ed3c1ad0d4fd1d1f2bd83d9e5981834
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-06-20 18:59:42 +0800

    update

--- utils/webdav/webdav.sh
+++ utils/webdav/webdav.sh
@@ -2,22 +2,32 @@
 set -e
 
 # set env
-USER=nobody
-GROUP=users
-GROUPS=nobody
+USER=webdav
+GROUP=webdav
+GROUPS=webdav,users
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 UMASK=${UMASK:-000}
+CONFIG_FILE="/data/config.yml"
 
 # set user to specified user id (non unique)
-usermod -o -u ${PUID} -g ${GROUP} -G ${GROUPS} -s /bin/ash ${USER} &>/dev/null
+usermod -o -u ${PUID} -g ${GROUP} -aG ${GROUPS} -s /bin/ash ${USER} &>/dev/null
 
 # set group users to specified group id (non unique)
 groupmod -o -g ${PGID} ${GROUP} &>/dev/null
 
+# copy and chown config.yml
+if [ ! -f "${CONFIG_FILE}" ]; then
+  echo "config.yml does not exist, copy config.yml"
+  cp -a /opt/config.yml /data/
+else
+  echo "config.yml file already exists, skip copy"
+fi
+
 #set folder's owne
 chown $PUID:$PGID /data
 chown $PUID:$PGID /etc/webdav
+chown $PUID:$PGID ${CONFIG_FILE}
 
 # if UMASK is not null set umask
 if [ "x$UMASK" != "x" ]; then
@@ -25,5 +35,4 @@ if [ "x$UMASK" != "x" ]; then
 fi
 
 # exec su -s /bin/ash ${USER} -c umask $UMASK -c webdav "$@"
-gosu ${USER} sh -c "umask ${UMASK} && webdav -c $0" "$@"
-# gosu ${USER} sh -c "umask ${UMASK} && webdav -c $0" "$@"
+gosu ${USER} sh -c "umask ${UMASK} && webdav -c ${CONFIG_FILE}" "$@"
