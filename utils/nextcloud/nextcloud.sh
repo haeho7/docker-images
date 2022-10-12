3a9ac4a3eae04e20ef34c51276daec83364e2576
haeho7 <40952365+haeho7@users.noreply.github.com>
2022-10-12 17:01:01 +0800

    fix(nextcloud build): refine code
    Dockerfile
      - optimize the build process
      - delete redundant comments
      - remove "replace rsync_options env" code, because nextcloud 24.0.5 already fixed.
      - remove "chown nextcloud folder" code
      - update nextcloud version to 24.0.6-fpm-alpine
    
    nextcloud.sh
      - remove "chown nextcloud folder" code

--- utils/nextcloud/nextcloud.sh
+++ utils/nextcloud/nextcloud.sh
@@ -14,10 +14,6 @@ usermod -o -u ${PUID} -g ${GROUP} -aG ${GROUPS} -s /bin/ash ${USER} &>/dev/null
 # set group users to specified group id (non unique)
 groupmod -o -g ${PGID} ${GROUP} &>/dev/null
 
-# chown nextcloud folder
-# See more: https://github.com/nextcloud/docker/blob/00d88733d0d93d0abc628c7b18bc589181c34cbb/24/fpm-alpine/Dockerfile#L100
-# cd /var/www/html && ls -1 /var/www/html | grep -v data | xargs chown -R ${PUID}:${PGID}
-
 # call nextcloud official crontab script
 if [ ! `pgrep -f "crond"` >/dev/null ]; then
   echo "crond process not runing, starting crond..."
