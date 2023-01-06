049b5c4172461b8e78ef52d40b955ceafd9b2df2
haeho7 <40952365+haeho7@users.noreply.github.com>
2023-01-06 18:48:07 +0800

    alist: update

--- utils/alist/alist.sh
+++ utils/alist/alist.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env sh
+#!/bin/sh
 set -e
 
 USER=alist
@@ -8,15 +8,19 @@ PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 UMASK=${UMASK:-022}
 
-_set_user_info() {
+_setup_user_info() {
   usermod -o -u ${PUID} -g ${GROUP} -aG ${GROUPS} -s /bin/ash ${USER}
   groupmod -o -g ${PGID} ${GROUP}
   umask ${UMASK}
 }
 
+_setup_owne () {
+  chown -R ${PUID}:${PGID} /opt/alist/
+}
+
 start_alist() {
   _set_user_info
-  chown -R ${PUID}:${PGID} /opt/alist/
+  _setup_owne
   exec gosu ${PUID}:${PGID} alist server --no-prefix
   #exec gosu ${PUID}:${PGID} sh -c "umask ${UMASK} && alist server --no-prefix"
 }
